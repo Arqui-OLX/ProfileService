f0bfee806060f95769059ac567961ba36ebc2e4f
Jhonatan R.Saumeth <jhriverasa@unal.edu.co>
2019-10-06 16:48:50 -0500

    CRUD

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+// Imports
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//Import routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -12,8 +14,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
+
+//Routes Definition
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
+const dbConn = require('../database/db')
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  dbConn.query('SELECT * FROM user', function (error, results, fields) {
+    if (error) throw error;
+    return res.send({ error: false, data: results, message: 'users list.' });
+  });
+});
+
+/* POST users listing. */
+router.post('/', function(req, res, next) {
+  let user = req.body;
+  dbConn.query('INSERT INTO user SET ?', user, function (error, results, fields) {
+    if (error) throw error;
+    return res.status(201).send({ error: false, data: results, message: user });
+  });
+});
+
+/* GET:id. */
+router.get('/:id', function(req, res, next) {
+  dbConn.query('SELECT * FROM user where id = ?',req.params.id, function (error, results, fields) {
+    if (error) throw error;
+    return res.send({ error: false, data: results, message: '.' });
+  });
+});
+
+/* UPDATE:id  (Passing whole user ) */
+router.put('/:id', function(req, res, next) {
+  let user = req.body;
+  let id = req.params.id
+  dbConn.query('UPDATE user SET ? where id = ?',[user , id], function (error, results, fields) {
+    if (error) throw error;
+    return res.send({ error: false, data: results, message: 'User updated' });
+  });
+});
+
+/* DELETE:id. */
+router.delete('/:id', function(req, res, next) {
+  dbConn.query('DELETE FROM user where id = ?',req.params.id, function (error, results, fields) {
+    if (error) throw error;
+    return res.send({ error: false, data: results, message: 'User deleted.' });
+  });
 });
 
 module.exports = router;
